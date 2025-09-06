05c8e508655c94ad4ce2d22bb122bd712e095de6
Jay17423 <jayprakash1200@gmail.com>
2025-09-06 12:00:54 +0530

    Create the update route for the post

--- server/app/routes/api/v1/post.py
+++ server/app/routes/api/v1/post.py
@@ -1,13 +1,13 @@
 # app/routers/post.py
 
 
-from fastapi import APIRouter, Depends, Form, File, UploadFile
+from fastapi import APIRouter, Depends, Form, File, HTTPException, UploadFile
 from sqlalchemy import Select
 from sqlmodel import Session, select
 from app.database import get_session # Assuming you have this
 from app.dependencies import get_current_user
 from app.models.post import Post
-from app.schema.post import PostRead
+from app.schema.post import PostRead, PostUpdate
 from app.services.post_services import create_post_with_image
 
 router = APIRouter(prefix="/posts", tags=["Posts"])
@@ -44,3 +44,29 @@ async def get_my_posts(
 
     return posts 
 
+@router.patch("/{id}", response_model=PostRead)
+def partial_update_post(
+    id: int, 
+    post: PostUpdate,
+    session: Session = Depends(get_session),
+    current_user: dict = Depends(get_current_user)
+):
+    db_post = session.exec(select(Post).where(Post.id == id)).first()
+    
+    if not db_post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    
+    if db_post.user_id != current_user["id"]:
+        raise HTTPException(status_code=403, detail="Not allowed to update this post")
+
+    if post.title is not None:
+        db_post.title = post.title
+    if post.content is not None:
+        db_post.content = post.content
+    if post.image_url is not None:
+        db_post.image_url = post.image_url
+
+    session.add(db_post)
+    session.commit()
+    session.refresh(db_post)
+    return db_post
