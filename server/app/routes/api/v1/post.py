be0372d6d2c5b9a44ff143695f0a1a830ca8187e
Jay17423 <jayprakash1200@gmail.com>
2025-09-06 11:42:50 +0530

    "Created get my post route

--- server/app/routes/api/v1/post.py
+++ server/app/routes/api/v1/post.py
@@ -1,9 +1,12 @@
 # app/routers/post.py
 
+
 from fastapi import APIRouter, Depends, Form, File, UploadFile
-from sqlmodel import Session
+from sqlalchemy import Select
+from sqlmodel import Session, select
 from app.database import get_session # Assuming you have this
 from app.dependencies import get_current_user
+from app.models.post import Post
 from app.schema.post import PostRead
 from app.services.post_services import create_post_with_image
 
@@ -28,3 +31,16 @@ async def create_post(
     )
 
     return post
+
+@router.get("/my-posts", response_model=list[PostRead])
+async def get_my_posts(
+    session: Session = Depends(get_session),
+    current_user: dict = Depends(get_current_user)
+):
+    statement = select(Post).where(Post.user_id == current_user["id"])
+    result = session.exec(statement).all()
+
+    posts = [p for (p,) in result] if result and isinstance(result[0], tuple) else result
+
+    return posts 
+
