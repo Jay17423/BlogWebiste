6101026e58044d65b99d5810d1fba7f577a1c26d
Jay17423 <jayprakash1200@gmail.com>
2025-09-07 23:33:00 +0530

    Impelemented search features

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -3,39 +3,42 @@ import NavBar from "./NavBar";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import FeedCard from "./FeedCard";
-import { Link } from "react-router-dom";
 
 const Homepage = () => {
   const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
   const token = useSelector((state) => state.user.token);
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
         const res = await axios.get("http://127.0.0.1:8000/api/v1/posts", {
-          headers: {
-            Authorization: `Bearer ${token}`, 
-          },
+          headers: { Authorization: `Bearer ${token}` },
         });
         setPosts(res.data);
+        setAllPosts(res.data); 
       } catch (err) {
         console.error("Error fetching posts:", err);
       }
     };
 
-    if (token) {
-      fetchPosts();
-    }
+    if (token) fetchPosts();
   }, [token]);
 
-
+  const onSearchResults = (results, query) => {
+    if (!query || query.trim() === "") {
+      setPosts(allPosts); 
+    } else {
+      setPosts(results);
+    }
+  };
 
   return (
     <div>
-      <NavBar></NavBar>
+      <NavBar onSearchResults={onSearchResults} />
       <div className="py-10 bg-gray-50 min-h-screen">
         {posts.map((post) => (
-          <FeedCard key={post.id} post={post}  />
+          <FeedCard key={post.id} post={post} />
         ))}
       </div>
     </div>
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { removeUser } from "../utils/userSlice";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
+import axios from "axios";
 
-const NavBar = () => {
+const NavBar = ({ onSearchResults }) => {
   const { username } = useSelector((state) => state?.user);
+  const [searchText, setSearchText] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const token = useSelector((state) => state.user.token);
 
   const handleLogout = () => {
     Cookies.remove("token");
     dispatch(removeUser());
     navigate("/login");
+  };
+
+  useEffect(() => {
+    if (searchText.trim() === "" && onSearchResults) {
+      onSearchResults([], "");
+    }
+  }, [searchText,onSearchResults]);
+
+  const onSearchHandler = async () => {
+    try {
+      if (!searchText.trim()) {
+        if (onSearchResults) onSearchResults([], "");
+        return;
+      }
+
+      const res = await axios.get(
+        `http://127.0.0.1:8000/api/v1/posts/filter-post?query=${searchText}`,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      if (onSearchResults) onSearchResults(res.data, searchText);
+    } catch (error) {
+      alert(error);
+    }
   };
 
   return (
@@ -24,6 +53,21 @@ const NavBar = () => {
         >
           BlogSite
         </h1>
+        <div className="flex justify-center items-center">
+          <input
+            placeholder="Search here"
+            type="search"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            className="bg-amber-50 p-2 rounded-2xl"
+          />
+          <button
+            className="text-2xl ml-3 cursor-pointer hover:scale-110 transition"
+            onClick={onSearchHandler}
+          >
+            🔎
+          </button>
+        </div>
 
         <ul className="flex space-x-8 text-white font-medium">
           <li
@@ -38,9 +82,9 @@ const NavBar = () => {
           >
             My Blogs
           </li>
-          <li className="hover:text-orange-200 cursor-pointer transition duration-200"
-          onClick={()=>navigate("/create-post")}
-          
+          <li
+            className="hover:text-orange-200 cursor-pointer transition duration-200"
+            onClick={() => navigate("/create-post")}
           >
             Create Blog
           </li>
